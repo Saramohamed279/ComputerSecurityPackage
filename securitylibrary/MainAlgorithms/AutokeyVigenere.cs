442b4e1312bcca30202dcc48f8f4797d513a4fc5
daniel-d7a <eyad.taym.saba@gmail.com>
2023-04-04 15:27:47 +0200

    Add project files.

---
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+
+
+    /*
+    Plain :  wearedisc overedsaveyourself
+    Cipher : zicvtwqng kzeiigasxstslvvwla
+    Key :    deciptive wearediscoveredsav
+
+
+    Key :    deciptivezicvtwqngkzeiigasx
+    Plain :  wearedisc
+    {wearedisclrcnpkkfr}
+     */
+
+    public class AutokeyVigenere : ICryptographicTechnique<string, string>
+    {
+
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public string Analyse(string plainText, string cipherText)
+        {
+            cipherText = cipherText.ToLower();  
+            var key = new StringBuilder();
+
+            for(int i = 0; i < plainText.Length; i++)
+            {
+                int ctIndex = alphabet.IndexOf(cipherText[i]);
+                int ptIndex = alphabet.IndexOf(plainText[i]);
+
+                char letter;
+                if(ctIndex - ptIndex  >= 0)
+                {
+                    letter = alphabet[(ctIndex - ptIndex)];
+                }
+                else
+                {
+                    letter = alphabet[(ctIndex - ptIndex + 26) % 26];
+                }
+
+                if (letter == plainText[0]) break;
+
+                key.Append(letter);
+            }
+
+            return key.ToString();
+        }
+
+        public string Decrypt(string cipherText, string key)
+        {
+            cipherText = cipherText.ToLower();
+
+            var actualKey = new StringBuilder(key);
+
+            var plaintext = new StringBuilder();
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int ctIndex = alphabet.IndexOf(cipherText[i]);
+                int keyIndex = alphabet.IndexOf(actualKey[i]);
+
+                char letter = alphabet[(ctIndex - keyIndex + 26) % 26];
+
+                plaintext.Append(letter);
+
+                if(actualKey.Length < cipherText.Length)
+                {
+                    actualKey.Append(letter);
+                }
+
+            }
+
+            return plaintext.ToString();
+        }
+
+        public string Encrypt(string plainText, string key)
+        {
+            int actualKeyLength = plainText.Length;
+
+            var actualKey = new StringBuilder(key);
+
+            int keyRange = actualKeyLength - key.Length; 
+
+            for(int i = 0; i < keyRange; i++)
+            {
+                actualKey.Append(plainText[i]);
+            }
+
+
+            var cipherText = new StringBuilder();
+
+            for(int i = 0; i < actualKeyLength; i++)
+            {
+                int ptIndex = alphabet.IndexOf(plainText[i]);
+                int keyIndex = alphabet.IndexOf(actualKey[i]);
+
+                char letter = alphabet[(ptIndex + keyIndex) % 26];
+
+                cipherText.Append(letter);
+
+            }
+
+            return cipherText.ToString();
+
+        }
+    }
+}
---
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+    public class Ceaser : ICryptographicTechnique<string, int>
+    {
+
+        int z = Convert.ToInt32('z');
+        int a = Convert.ToInt32('a');
+
+        public string Encrypt(string plainText, int key)
+        {
+            var cipher = new StringBuilder();
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+
+                int letter = Convert.ToInt32(Char.ToLower(plainText[i]));
+                int letter_with_key = letter + key;
+
+                char cipher_letter;
+
+                if(letter_with_key > z)
+                {
+                    cipher_letter = Convert.ToChar((Convert.ToInt32(letter_with_key) + a) % (z + 1));
+                }
+                else
+                {
+                    cipher_letter = Convert.ToChar(letter_with_key);
+                }
+
+                cipher.Append(cipher_letter);
+            }
+            return cipher.ToString();
+        }
+
+        public string Decrypt(string cipherText, int key)
+        {
+            var text = new StringBuilder();
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+
+                int letter = Convert.ToInt32(Char.ToLower(cipherText[i]));
+                int letter_with_key = letter - key;
+
+                char text_letter;
+
+                if (letter_with_key < a)
+                {
+                    text_letter = Convert.ToChar(Convert.ToInt32(letter_with_key) + z + 1 - a);
+                }
+                else
+                {
+                    text_letter = Convert.ToChar(letter_with_key);
+                }
+
+                text.Append(text_letter);
+            }
+            return text.ToString();
+        }
+
+        public int Analyse(string plainText, string cipherText)
+        {
+            for(int i = 0; i<26; i++) {
+
+                string test_cipher = Encrypt(plainText, i);
+
+                if (test_cipher.Equals(cipherText, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
