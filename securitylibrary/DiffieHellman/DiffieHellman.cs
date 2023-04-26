92ed718e7c11bf4c2d38294ba107a22f03dba571
Tarek Gamal <TarekMGamal1@gmail.com>
2023-04-26 23:19:34 +0200

    Added DiffieHelman Added remaining tests

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -8,9 +8,28 @@ namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman 
     {
+        static int modulus(int bas, int pow, int m)
+        {
+            int p = 1;
+            int a1 = bas % m;
+            for (int i = 1; i <= pow; i++)
+            {
+                p *= a1;
+                p = p % m;
+            }
+            return p;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+
+            //throw new NotImplementedException();
+            int Ya = modulus(alpha, xa, q);
+            int Yb = modulus(alpha, xb, q);
+            int Ka = modulus(Yb, xa, q);
+            int Kb = modulus(Ya, xb, q);
+            List<int> keys = new List<int> { Ka, Kb };
+            return keys;
+
         }
     }
 }
